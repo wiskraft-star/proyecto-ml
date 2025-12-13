fcd5f0062e252db8a1dd3a41a16cbdcaa533b147
wiskraft-star <wiskraft@gmail.com>
2025-12-13 17:55:53 -0300

    Delete proyecto-ml-ui directory

--- proyecto-ml-ui/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { env } from "./env";
-
-export const supabase = createClient(env.supabaseUrl, env.supabaseAnonKey, {
-  auth: { persistSession: false }
-});
